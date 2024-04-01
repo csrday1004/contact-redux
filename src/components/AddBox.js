ed63e57b8241a9712f6dfe6fac275a298ae5ac73
JSR <csrday1004@gmail.com>
2024-04-01 19:39:18 +0900

    랜덤친구 안내문구

--- src/components/AddBox.js
+++ src/components/AddBox.js
@@ -4,43 +4,43 @@ import { useDispatch } from "react-redux";
 import { randomUsername } from "korean-random-username";
 
 export const AddBox = () => {
-  
-  const dispatch = useDispatch() 
+  const dispatch = useDispatch();
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [profileImg,setProfileImg]=useState("https://source.boringavatars.com/beam")
-  const inputNameRef = useRef()
-  const inputPhoneNumberRef = useRef()
-  const [toggle, setToggle] = useState(true)
-  
-  useEffect(()=>{
-    if (!name) { 
-      setName(randomUsername())
+  const [profileImg, setProfileImg] = useState(
+    "https://source.boringavatars.com/beam"
+  );
+  const inputNameRef = useRef();
+  const inputPhoneNumberRef = useRef();
+  const [toggle, setToggle] = useState(true);
+
+  useEffect(() => {
+    if (!name) {
+      setName(randomUsername());
     }
-    if(!phoneNumber){
-      setPhoneNumber('010'+Math.floor(10000000 + Math.random() * 90000000))
+    if (!phoneNumber) {
+      setPhoneNumber("010" + Math.floor(10000000 + Math.random() * 90000000));
     }
-  },[toggle])
-  
+  }, [toggle]);
+
+  const addFriend = (e) => {
+    e.preventDefault();
+    let random = Math.random();
+    setProfileImg(`https://source.boringavatars.com/beam/${random}`);
+    dispatch({
+      type: "ADD_CONTACT",
+      payload: { name, phoneNumber, profileImg },
+    });
+    inputNameRef.current.value = null;
+    inputPhoneNumberRef.current.value = null;
+    setName("");
+    setPhoneNumber("");
+    setToggle(!toggle);
+  };
+
   return (
-    <Form
-    onSubmit={(e) => {
-      e.preventDefault();
-      let random = Math.random()
-      setProfileImg(`https://source.boringavatars.com/beam/${random}`)
-      dispatch({type:"ADD_CONTACT", payload:{name,phoneNumber,profileImg}})
-      inputNameRef.current.value=null
-      inputPhoneNumberRef.current.value=null
-      setName("")
-      setPhoneNumber("")
-      setToggle(!toggle)
-    }}
-    >
-      <Form.Group
-        className="mb-3"
-        controlId="formBasicEmail"
-        
-      >
+    <Form onSubmit={(e)=>{addFriend(e)}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>이름</Form.Label>
         <Form.Control
           type="text"
@@ -66,6 +66,7 @@ export const AddBox = () => {
       <Button variant="primary" type="submit">
         연락처 추가
       </Button>
+      <span style={{fontSize:'12px', paddingLeft:'5px'}}> *입력 없이 누르면 랜덤 친구 생성</span>
     </Form>
   );
 };
